c9564df0f6e0b16ae206372084fcbab3fc8975c0
Rasmus Kaj <kaj@kth.se>
2017-01-27 21:53:22 +0100

    Move static out of templates dir.

--- examples/statics/src/build.rs
+++ examples/statics/src/build.rs
@@ -6,9 +6,7 @@ use std::path::PathBuf;
 
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let in_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
-        .join("templates");
-    compile_static_css(&in_dir.join("static"), &out_dir)
-        .expect("Compile static css");
-    compile_templates(&in_dir, &out_dir).expect("foo");
+    let in_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    compile_static_css(&in_dir.join("static"), &out_dir).unwrap();
+    compile_templates(&in_dir.join("templates"), &out_dir).unwrap();
 }
