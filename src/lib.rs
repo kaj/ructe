8c17998cd07472dba31bf8e10f7e90b8e8e92337
Kornel <kornel@geekhood.net>
2018-05-06 12:55:44 +0100

    Fix MIME type

--- src/lib.rs
+++ src/lib.rs
@@ -454,7 +454,7 @@ fn mime_from_suffix(suffix: &str) -> &'static str {
         "js" => "application/javascript",
         "png" => "image/png",
         "woff" => "application/font-woff",
-        _ => "Application/OctetStream",
+        _ => "application/octet-stream",
     }
 }
 
