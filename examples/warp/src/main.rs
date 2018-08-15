b811b52fbfda000e94f476e0501f1d8a210a9104
Rasmus Kaj <kaj@kth.se>
2018-08-15 11:07:33 +0200

    Improve error handling in warp example.

--- examples/warp/src/main.rs
+++ examples/warp/src/main.rs
@@ -7,31 +7,32 @@ use std::io::{self, Write};
 use std::time::{Duration, SystemTime};
 use templates::statics::StaticFile;
 use warp::http::{Response, StatusCode};
-use warp::{Filter, Rejection, Reply};
+use warp::{path, reject, Filter, Rejection, Reply};
 
 /// Main program: Set up routes and start server.
 fn main() {
     env_logger::init();
 
-    let index = warp::index().map(home_page);
-    let statics = warp::path("static")
-        .and(warp::path::param())
-        .map(static_file);
+    let index = warp::index().and_then(home_page);
+    let statics = path("static").and(path::param()).and_then(static_file);
+    let bad = path("bad").and_then(bad_handler);
 
-    let route = warp::get2().and(index.or(statics)).recover(customize_error);
+    let route = warp::get2()
+        .and(index.or(statics).or(bad))
+        .recover(customize_error);
     warp::serve(route).run(([127, 0, 0, 1], 3030));
 }
 
-fn home_page() -> impl warp::Reply {
+fn home_page() -> Result<impl Reply, Rejection> {
     use templates::page;
     let mut buf = Vec::new();
-    match page(&mut buf, &[("first", 3), ("second", 7), ("third", 2)]) {
-        Ok(()) => String::from_utf8(buf).unwrap(),
-        Err(e) => {
-            println!("Rendering failed: {}", e);
-            "error".into()
-        }
-    }
+    page(&mut buf, &[("first", 3), ("second", 7), ("third", 2)])
+        .map_err(|_| reject::server_error())?;
+    Ok(String::from_utf8(buf).unwrap())
+}
+
+fn bad_handler() -> Result<StatusCode, Rejection> {
+    Err(reject::server_error())
 }
 
 /// This method can be used as a "template tag", that is a method that
@@ -50,20 +51,17 @@ fn footer(out: &mut Write) -> io::Result<()> {
 /// The state will contain a FilePath.  The response from this view
 /// should be the file data with a correct content type and a far
 /// expires header (or a 404 if the file does not exist).
-fn static_file(name: String) -> Result<impl Reply, warp::http::Error> {
+fn static_file(name: String) -> Result<impl Reply, Rejection> {
     if let Some(data) = StaticFile::get(&name) {
         let _far_expires = SystemTime::now() + FAR;
-        Response::builder()
+        Ok(Response::builder()
             .status(StatusCode::OK)
             .header("content-type", data.mime.as_ref())
             // TODO .header("expires", _far_expires)
-            .body(data.content)
+            .body(data.content))
     } else {
         println!("Static file {} not found", name);
-        Response::builder()
-            .status(StatusCode::NOT_FOUND)
-            .header("content-type", "text/plain")
-            .body(&b"not found"[..])
+        Err(reject::not_found())
     }
 }
 
@@ -73,35 +71,22 @@ static FAR: Duration = Duration::from_secs(180 * 24 * 60 * 60);
 fn customize_error(err: Rejection) -> Result<impl Reply, Rejection> {
     match err.status() {
         StatusCode::NOT_FOUND => {
-            let mut buf = Vec::new();
-            use templates::not_found;
-            match not_found(&mut buf) {
-                Ok(()) => Ok(Response::builder()
-                    .status(StatusCode::NOT_FOUND)
-                    .body(buf)),
-                Err(e) => {
-                    println!("Rendering failed: {}", e);
-                    Ok(Response::builder()
-                        .status(StatusCode::NOT_FOUND)
-                        .body(
-                            b"you get a 404, and *you* get a 404...".to_vec(),
-                        ))
-                }
-            }
+            eprintln!("Got a 404: {:?}", err);
             // We have a custom 404 page!
+            let mut buf = Vec::new();
+            templates::error(
+                &mut buf,
+                StatusCode::NOT_FOUND,
+                "The resource you requested could not be located.",
+            ).map_err(|_| err)?;
+            Ok(Response::builder().status(StatusCode::NOT_FOUND).body(buf))
         }
-        StatusCode::INTERNAL_SERVER_ERROR => {
-            // Oh no, something is on fire!
-            eprintln!("quick, page someone! fire!");
-            Ok(Response::builder()
-                .status(StatusCode::INTERNAL_SERVER_ERROR)
-                .body(b":fire: this is fine".to_vec()))
-        }
-        _ => {
-            // Don't customize these errors, just let warp do
-            // the default! (or optionally a later filter could
-            // customize these).
-            Err(err)
+        code => {
+            eprintln!("Got a {}: {:?}", code.as_u16(), err);
+            let mut buf = Vec::new();
+            templates::error(&mut buf, code, "Something went wrong.")
+                .map_err(|_| err)?;
+            Ok(Response::builder().status(code).body(buf))
         }
     }
 }
