07f5f1c6408c697d8a47f187cae9966c64431222
Rasmus Kaj <kaj@kth.se>
2020-08-06 23:57:38 +0200

    Add some documentation.

--- examples/tide/src/ructe_tide.rs
+++ examples/tide/src/ructe_tide.rs
@@ -1,8 +1,47 @@
+//! These traits and impl may be included in the tide feature of ructe
+//! in a future release.
+//!
+//! Comments welcome at
+//! [kaj/ructe#79](https://github.com/kaj/ructe/issues/79).
+
+/// Add `render` and `render_html` methods to [`tide::Response`].
+///
+/// [`tide::Response`]: ../../tide/struct.Response.html
 pub trait Render {
+    /// Render a template to the body of self.
+    ///
+    /// The `Call` takes a `Write` target as only argument, other
+    /// arguments will typically be moved or borrowed into a closure
+    /// that is used as `call`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use tide::Response;
+    /// # use ructe_tide::ructe_tide::Render;
+    /// # use std::io::{self, Write};
+    /// # // Mock template:
+    /// # fn page(o: impl Write, c: &str, n: u8) -> io::Result<()> {
+    /// #     Ok(())
+    /// # }
+    /// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+    /// let content = "something";
+    /// let other = 17;
+    /// let mut result = Response::new(200);
+    /// result.render(|o| page(o, content, other))?;
+    /// # Ok(())
+    /// # }
+    /// ```
     fn render<Call>(&mut self, call: Call) -> std::io::Result<()>
     where
         Call: FnOnce(&mut dyn std::io::Write) -> std::io::Result<()>;
 
+    /// Render a template to the html body of self.
+    ///
+    /// just like `render`, excep it also sets the content-type of the
+    /// respons to [`HTML`].
+    ///
+    /// [`HTML`]: ../../tide/http/mime/constant.HTML.html
     fn render_html<Call>(&mut self, call: Call) -> std::io::Result<()>
     where
         Call: FnOnce(&mut dyn std::io::Write) -> std::io::Result<()>;
@@ -29,11 +68,43 @@ impl Render for tide::Response {
     }
 }
 
+/// Add `render` and `render_html` methods to [`tide::ResponseBuilder`].
+///
+/// [`tide::Response`]: ../../tide/struct.ResponseBuilder.html
 pub trait RenderBuilder {
+    /// Render a template to the body of self.
+    ///
+    /// The `Call` takes a `Write` target as only argument, other
+    /// arguments will typically be moved or borrowed into a closure
+    /// that is used as `call`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use tide::Response;
+    /// # use ructe_tide::ructe_tide::RenderBuilder;
+    /// # use std::io::{self, Write};
+    /// # // Mock template:
+    /// # fn page(o: impl Write, c: &str, n: u8) -> io::Result<()> {
+    /// #     Ok(())
+    /// # }
+    /// let content = "something";
+    /// let other = 17;
+    /// Response::builder(200)
+    ///     .render(|o| page(o, content, other))
+    ///     .build()
+    /// # ;
+    /// ```
     fn render<Call>(self, call: Call) -> tide::ResponseBuilder
     where
         Call: FnOnce(&mut dyn std::io::Write) -> std::io::Result<()>;
 
+    /// Render a template to the html body of self.
+    ///
+    /// just like `render`, excep it also sets the content-type of the
+    /// respons to [`HTML`].
+    ///
+    /// [`HTML`]: ../../tide/http/mime/constant.HTML.html
     fn render_html<Call>(self, call: Call) -> tide::ResponseBuilder
     where
         Call: FnOnce(&mut dyn std::io::Write) -> std::io::Result<()>;
