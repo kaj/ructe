8174887a51bdac1ceaaff9310ebeae41fabbb224
Rasmus Kaj <kaj@kth.se>
2023-01-22 14:27:03 +0100

    Clean up the benchmark.

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,119 +2,98 @@
 extern crate test;
 use ructe::templates::{Html, ToHtml};
 use std::io::Write;
-use test::Bencher;
+use test::bench::{Bencher, black_box};
 
 #[bench]
 fn raw(b: &mut Bencher) {
-    let mut buf = Vec::with_capacity(10000);
+    let mut buf = Vec::with_capacity(500);
     b.iter(|| {
         buf.clear();
-        raw_inner(&mut buf);
-        buf.len() // prevents optimizing writes out
+        black_box(Html("Hello <World>").to_html(&mut buf)).unwrap();
     });
 }
-
-/// real template is a function that takes a generic `Write` parameter, so non-inlineable
-/// function should simulate that instead of allowing optimizer to specialize for `Vec`
-#[inline(never)]
-fn raw_inner(buf: &mut impl Write) {
-    // inner loop to stress escaping more than buffer allocation
-    for _ in 0..1000 {
-        let h = Html("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod");
-        h.to_html(buf).unwrap();
-    }
+#[bench]
+fn raw_baseline(b: &mut Bencher) {
+    let mut buf = Vec::with_capacity(500);
+    b.iter(|| {
+        buf.clear();
+        black_box(write!(&mut buf, "{}", "Hello <World>")).unwrap();
+    });
 }
 
 #[bench]
 fn escaped_no_op(b: &mut Bencher) {
-    let mut buf = Vec::with_capacity(10000);
+    let mut buf = Vec::with_capacity(500);
     b.iter(|| {
         buf.clear();
-        escaped_no_op_inner(&mut buf);
-        buf.len()
+        black_box("Hello World!!".to_html(&mut buf)).unwrap();
     });
 }
 
-#[inline(never)]
-fn escaped_no_op_inner(buf: &mut impl Write) {
-    let h = "hello world";
-    for _ in 0..1000 {
-        h.to_html(buf).unwrap();
-        h.to_html(buf).unwrap();
-        h.to_html(buf).unwrap();
-    }
-}
-
+/// Note: Numbers can't contain anything that should be escaped, so
+/// with proper specialization, this should be as fast as
+/// escaped_nums_h and escaped_nums_baseline.
 #[bench]
 fn escaped_nums(b: &mut Bencher) {
-    let mut buf = Vec::with_capacity(10000);
+    let mut buf = Vec::with_capacity(500);
     b.iter(|| {
         buf.clear();
-        escaped_nums_inner(&mut buf);
-        buf.len()
+        black_box(12_345.to_html(&mut buf)).unwrap();
     });
 }
 
-#[inline(never)]
-fn escaped_nums_inner(buf: &mut impl Write) {
-    for i in 0..1000 {
-        i.to_html(buf).unwrap();
-        5.to_html(buf).unwrap();
-        i.to_html(buf).unwrap();
-    }
+/// Turn of escapeing for the number by claiming it to be pre-escaped.
+#[bench]
+fn escaped_nums_h(b: &mut Bencher) {
+    let mut buf = Vec::with_capacity(500);
+    b.iter(|| {
+        buf.clear();
+        black_box(Html(12_345).to_html(&mut buf)).unwrap();
+    });
+}
+
+/// Raw rust write of the same number for comparision.
+#[bench]
+fn escaped_nums_baseline(b: &mut Bencher) {
+    let mut buf = Vec::with_capacity(500);
+    b.iter(|| {
+        buf.clear();
+        black_box(write!(&mut buf, "{}", 12_345)).unwrap();
+    });
 }
 
 #[bench]
 fn escaped_short_impl(b: &mut Bencher) {
-    let mut buf = Vec::with_capacity(10000);
+    let mut buf = Vec::with_capacity(500);
     b.iter(|| {
         buf.clear();
-        escaped_short_inner(&mut buf);
-        buf.len()
+        black_box("Hello <World>".to_html(&mut buf)).unwrap();
     });
 }
 
 #[bench]
 fn escaped_short_dyn(b: &mut Bencher) {
-    let mut buf = Vec::with_capacity(10000);
+    let mut buf = Vec::with_capacity(500);
     b.iter(|| {
         buf.clear();
-        let mut dynwrite: &mut dyn Write = &mut buf;
-        escaped_short_inner(&mut dynwrite);
-        buf.len()
+        let mut dynwrite: &mut dyn Write = black_box(&mut buf);
+        black_box("Hello <World>".to_html(&mut dynwrite)).unwrap();
     });
-}
-
-#[inline(never)]
-fn escaped_short_inner(buf: &mut impl Write) {
-    for _ in 0..1000 {
-        "hello&world".to_html(buf).unwrap();
-        "hi".to_html(buf).unwrap();
-        "hello=world!".to_html(buf).unwrap();
-    }
 }
 
 #[bench]
 fn escaped_long(b: &mut Bencher) {
-    let mut buf = Vec::with_capacity(10000);
+    let mut buf = Vec::with_capacity(1000);
+    let text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, \
+                sed do eiusmod tempor incididunt ut labore et dolore magna \
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.\n \
+                Duis aute irure dolor in reprehenderit <in> voluptate velit \
+                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint \
+                occaecat cupidatat non proident, sunt in culpa qui officia \
+                deserunt mollit anim id est laborum.\n";
     b.iter(|| {
         buf.clear();
-        escaped_long_inner(&mut buf);
-        buf.len()
+        black_box(text.to_html(&mut buf)).unwrap();
     });
-}
-
-#[inline(never)]
-fn escaped_long_inner(buf: &mut impl Write) {
-    for _ in 0..100 {
-        let h = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit <in> voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-        h.to_html(buf).unwrap();
-        h.to_html(buf).unwrap();
-        h.to_html(buf).unwrap();
-    }
 }
